a5545da5f5d6167cb5b01ec82219fa1f1b101b30
whitemousess <thangcoiofficial@gmail.com>
2024-01-29 10:52:26 +0700

    up

--- src/Controllers/removeController.ts
+++ src/Controllers/removeController.ts
@@ -3,13 +3,14 @@ import { removeBackground } from "@imgly/background-removal-node";
 
 const removeBG = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const imagePath = req.file!.buffer;
-    const blobData = await removeBackground(imagePath);
-    res.json(blobData);
-    // const buffer = await blobData.arrayBuffer();
-    // const base64String = Buffer.from(buffer).toString("base64");
+    if (req.file) {
+      const imagePath = req.file!.buffer;
+      const blobData = await removeBackground(imagePath);
+      const buffer = await blobData.arrayBuffer();
+      const base64String = Buffer.from(buffer).toString("base64");
 
-    // res.json(base64String);
+      res.json(base64String);
+    }
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
   }
